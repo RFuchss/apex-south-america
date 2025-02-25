c7502a89808169c9cb92cc7fa3035fb84378fb02
Rafael Fuchs <rafael.fuchs@simpledatacorp.com>
2025-02-25 00:09:13 -0400

    segundo commit jeje

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,3 @@
-/*!
-=========================================================
-* Ollie Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
-// smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
@@ -30,35 +14,25 @@ $(document).ready(function(){
             });
         } 
     });
-});
 
-// portfolio carousel
-$('#owl-portfolio').owlCarousel({
-    margin:30,
-    dots: false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:4,
-            nav:false,
-            loop:false
+    $('#owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items:1
+            },
+            480: {
+                items:3
+            },
+            768: {
+                items:5
+            }
         }
-    }
+    });
+    $('#owl-carousel').trigger('play.owl.autoplay',[1000]);
 });
-
-// testmonial carousel
-$('#owl-testmonial').owlCarousel({
-    center: true,
-    items:1,
-    loop:true,
-    nav: true,
-    dots: false
-})
